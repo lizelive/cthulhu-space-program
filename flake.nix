ec41c49b77ec412ede80d46d829c776739411547
Ada Rosa <ada.rosa@microsoft.com>
2022-11-26 01:17:49 -0800

    use nightly

--- flake.nix
+++ flake.nix
@@ -9,21 +9,31 @@
   outputs = { self, nixpkgs, utils, naersk, nixpkgs-mozilla }:
     utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system;
-
+        pkgs = import nixpkgs {
+          overlays = [
+            nixpkgs-mozilla.overlay
+          ];
+          inherit system;
         };
+        # pkgs.latest.rustChannels.nightly.rust
         toolchain = (pkgs.rustChannelOf {
           rustToolchain = ./rust-toolchain;
-          sha256 = "";
+          sha256 = "sha256-s6XVvROGjTKvbWQ/zjM3hPv15CcF4Uj9lDI5RfxZsnA=";
           #        ^ After you run `nix-build`, replace this with the actual
           #          hash from the error message
         }).rust;
-        naersk-lib = pkgs.callPackage naersk { };
+        naersk-lib = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
       in
       {
-        defaultPackage = naersk-lib.buildPackage ./.;
+        defaultPackage = with pkgs; naersk-lib.buildPackage {
+          buildInputs = [ libudev-zero alsa-lib pkg-config ];
+          src = ./.;
+        };
         devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy gcc libudev-zero alsa-lib pkg-config ];
+          buildInputs = [ toolchain cargo rustc rustfmt pre-commit rustPackages.clippy gcc libudev-zero alsa-lib pkg-config ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
       });
